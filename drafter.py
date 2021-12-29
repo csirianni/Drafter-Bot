59a26ac26f2c7785508ce0eb173b3fab504abe13
Cedric Sirianni <cds1@sirianni.org>
2021-12-29 12:42:16 -0500

    convert drafter file to OOP

--- drafter.py
+++ drafter.py
@@ -1,27 +1,57 @@
 # import random module
 import random
 
-# generate list of civilizations excluding bans
-def bans(civs, bans):
-    for ban in bans:
-        civs.remove(ban)
-    return civs
+# import csv module
+import csv
 
-# reformat list into words separated by commas
-def format_picks(picks):
-    return ", ".join(picks)
+class Drafter:
+    # open csv file
+    with open('civ-list.csv') as f:
+        # create a reader object
+        reader = csv.reader(f)
+        # initialize empty list for civ names
+        full_civ_list = []
+        # iterate over reader and append civ name to civs list
+        for civ in reader:
+            # index 0 used to unwrap list containing the civ name
+            full_civ_list.append(civ[0])
 
-# generate the picks for each person
-def draft(civs, names):
-    for name in names:
-        # initialize empty list for civ picks
-        picks = []
-        for i in range(3):
-            # length of civs list
-            num_of_civs = len(civs)
-            # select random civ and remove it from list
-            pick = civs.pop(random.randint(0, num_of_civs - 1))
-            # add civ to list of picks
-            picks.append(pick)
-        # print picks for each person
-        print(name + ": " + format_picks(picks))
+    def __init__(self, name_list: list, civ_list=full_civ_list):
+        self.name_list = name_list
+        self.civ_list = civ_list
+
+    # generate list of civilizations excluding bans
+    def ban(self, civ: str):
+        self.civ_list.remove(civ)
+        return self.civ_list
+
+    # reformat list into words separated by commas
+    def format_picks(picks):
+        return ", ".join(picks)
+
+    # generate the picks for each person
+    def draft(self):
+        for name in self.name_list:
+            # initialize empty list for civ picks
+            picks = []
+            for i in range(3):
+                # length of civs list
+                num_of_civs = len(self.civ_list)
+                # select random civ and remove it from list
+                pick = self.civ_list.pop(random.randint(0, num_of_civs - 1))
+                # add civ to list of picks
+                picks.append(pick)
+            # return picks for each person
+            return name + ": " + self.format_picks(picks)
+
+    def set_name_list(self, name_list):
+        self.name_list = name_list
+
+    def get_name_list(self):
+        return self.name_list
+    
+    def set_civ_list(self, civ_list):
+        self.civ_list = civ_list
+    
+    def get_civ_list(self):
+        return self.civ_list
