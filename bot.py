d11171a53f3cd23ab583988000449984a1ed1dcc
Cedric Sirianni <cds1@sirianni.org>
2021-12-30 01:38:25 -0500

    remove env token process

--- bot.py
+++ bot.py
@@ -4,16 +4,11 @@ from discord.ext import commands
 
 # used to get token from .env file
 import os
-from dotenv import load_dotenv
 
 # import csv module
 import csv
 
 import drafter
-
-# get token
-load_dotenv()
-TOKEN = os.getenv('TOKEN')
 
 # enable accessing members
 intents = discord.Intents.default()
@@ -142,4 +137,4 @@ async def help(ctx):
     embed = discord.Embed(title="Drafter Commands", description=f"- `{prefix}start`: Initialize the list of players and the list of civs.\n- `{prefix}ban {{civ_name}}`: Remove specified civ from list of draftable civs. Limited to 2 per player. \n- `{prefix}draft`: Generate the civ picks for each person.\n- `{prefix}civlist`: Print the list of draftable civs.\n- `{prefix}playerlist`: Print the list of players who will be assigned civ picks.\n- `{prefix}removeplayer {{player_name}}`: Remove specified player from list of players who will be assigned civ picks. \n- `{prefix}addplayer {{player_name}}`: Add specified player to list of players who will be assigned civ picks.", color=discord.Color.blue())
     await ctx.send(embed=embed)
 
-bot.run(TOKEN)
+bot.run("OTI1NTM1MTUwMTE1MjY2NjMw.Ycuhxw.j4tCN3vkEZ6PDmVJ6mW1vtFW6-4")
