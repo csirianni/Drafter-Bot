76e387e54c9fa2b639bf9872bccffe5b67e3c5be
Cedric Sirianni <cds1@sirianni.org>
2022-01-01 17:18:39 -0500

    add some type hinting

--- drafter.py
+++ drafter.py
@@ -9,19 +9,18 @@ class Drafter:
         self.civ_list = civ_list
         self.player_bans = player_bans
 
-    # remove civ from list of civs and return removed civ
-    def ban(self, civ: str):
+    # remove civ from list of civs and return the removed civ
+    def ban(self, civ: str) -> str:
         lower_civ_list = list(map(lambda civ: civ.lower(), self.civ_list))
         index = lower_civ_list.index(civ.lower())
-        removed_civ = self.civ_list.pop(index)
-        return removed_civ
+        return self.civ_list.pop(index)
 
     # reformat list into words separated by commas
-    def format_list(self,the_list):
+    def format_list(self, the_list: list) -> list:
         return ", ".join(the_list)
 
     # generate the picks for each person
-    def draft(self):
+    def draft(self) -> str:
         msg = ""
         for player in self.player_list:
             # initialize empty list for civ picks
@@ -40,17 +39,17 @@ class Drafter:
     def set_player_list(self, player_list):
         self.player_list = player_list
 
-    def get_player_list(self):
+    def get_player_list(self) -> list:
         return self.format_list(self.player_list)
     
     def set_civ_list(self, civ_list):
         self.civ_list = civ_list
 
-    def get_civ_list(self):
+    def get_civ_list(self) -> list:
         return self.format_list(self.civ_list)
 
     def set_player_bans(self, player_bans):
         self.player_bans = player_bans
     
-    def get_player_bans(self):
+    def get_player_bans(self) -> list:
         return self.player_bans
