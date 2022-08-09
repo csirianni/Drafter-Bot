66b8fad8fa795ca5426647f94d1d32dcd2553c37
Cedric Sirianni <cds1@sirianni.org>
2022-08-09 19:35:21 -0400

    fix import name and object name being the same

--- bot.py
+++ bot.py
@@ -3,15 +3,16 @@ import copy
 from discord.errors import HTTPException
 from discord.ext import commands
 
-# used to get token from .env file
-import os
-TOKEN = os.environ['TOKEN']
-
 # import csv module
 import csv
 
-# import Drafter methods
-import drafter
+# import Drafter constructor
+from drafter import Drafter
+
+# used to get token from .env file
+import os
+
+TOKEN = os.environ['TOKEN']
 
 # enable accessing members
 intents = discord.Intents.default()
@@ -22,16 +23,19 @@ prefix = '!'
 activity = discord.Activity(type=discord.ActivityType.listening, name=f"{prefix}help")
 bot = commands.Bot(command_prefix=prefix, intents=intents, help_command=None, activity=activity)
 
+
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
 
+
 # initialize Drafter object with empty player_list, civ_list, player_bans and ban_limit = 0
-drafter = drafter.Drafter([], [], {}, 0)
-redraft = drafter.Drafter([], [], {}, 0)
+drafter = Drafter([], [], {}, 0)
+drafter_copy = Drafter([], [], {}, 0)
+
 
 @bot.command()
-async def start(ctx, ban_limit: int = None):    
+async def start(ctx, ban_limit: int = None):
     # store guild object of guild where command is used
     guild = ctx.guild
     # initialize list of players (excluding bots)
@@ -39,7 +43,8 @@ async def start(ctx, ban_limit: int = None):
     player_display_names = [member.display_name for member in guild.members if not member.bot]
     # ensure the list contains only unique display_names
     if len(player_display_names) != len(set(player_display_names)):
-        await ctx.send(f"Error! `{prefix}start {{ban_limit}} (optional)` expected  each player to have a unique name. Try again.")
+        await ctx.send(
+            f"Error! `{prefix}start {{ban_limit}} (optional)` expected  each player to have a unique name. Try again.")
     # ensure the ban limit > 0
     elif ban_limit is not None and ban_limit < 0:
         await ctx.send(f"Error! Invalid ban limit. It must be non-negative. Try again.")
@@ -69,9 +74,10 @@ async def start(ctx, ban_limit: int = None):
             drafter.set_ban_limit(2)
         else:
             drafter.set_ban_limit(ban_limit)
-        
+
         msg = f"Starting the draft! The ban limit is {drafter.ban_limit} per person. Here's the list of players: " + drafter.get_player_list()
         await ctx.send(msg)
+
 
 @bot.command()
 async def ban(ctx, civ: str):
@@ -92,31 +98,39 @@ async def ban(ctx, civ: str):
                 await ctx.send(f"**{civ}** is not a valid civilization. Try again.")
         else:
             if drafter.ban_limit < 2:
-                await ctx.send(f"**{ctx.author.display_name}**, you've already banned {drafter.ban_limit} civilization!")
+                await ctx.send(
+                    f"**{ctx.author.display_name}**, you've already banned {drafter.ban_limit} civilization!")
             else:
-                await ctx.send(f"**{ctx.author.display_name}**, you've already banned {drafter.ban_limit} civilizations!")
+                await ctx.send(
+                    f"**{ctx.author.display_name}**, you've already banned {drafter.ban_limit} civilizations!")
     except KeyError:
-        await ctx.send(f"**{ctx.author.display_name}**, you're not in the list of players. Make sure you ran `{prefix}start {{ban_limit}} (optional)` first. If you have, try using `{prefix}addplayer {{player_name}}` to add yourself to the list.")
+        await ctx.send(
+            f"**{ctx.author.display_name}**, you're not in the list of players. Make sure you ran `{prefix}start {{ban_limit}} (optional)` first. If you have, try using `{prefix}addplayer {{player_name}}` to add yourself to the list.")
+
 
 @bot.command()
 async def draft(ctx):
-    redraft = copy.deepcopy(drafter)
+    drafter_copy = copy.deepcopy(drafter)
     msg = drafter.draft()
     try:
         await ctx.send(msg)
     except HTTPException:
         # this occurs when player_list is empty; ctx.send can't send an empty message
-        await ctx.send(f"HTTPException: The player list is empty. Make sure you ran `{prefix}start {{ban_limit}} (optional)` first.")
+        await ctx.send(
+            f"HTTPException: The player list is empty. Make sure you ran `{prefix}start {{ban_limit}} (optional)` first.")
+
 
 @bot.command()
 async def reroll(ctx):
-    drafter = copy.deepcopy(redraft)
+    drafter = copy.deepcopy(drafter_copy)
     msg = drafter.draft()
     try:
         await ctx.send(msg)
     except HTTPException:
         # this occurs when player_list is empty; ctx.send can't send an empty message
-        await ctx.send(f"HTTPException: The player list is empty. Make sure you ran `{prefix}start {{ban_limit}} (optional)` first.")        
+        await ctx.send(
+            f"HTTPException: The player list is empty. Make sure you ran `{prefix}start {{ban_limit}} (optional)` first.")
+
 
 @bot.command()
 async def civlist(ctx):
@@ -124,7 +138,9 @@ async def civlist(ctx):
         await ctx.send(drafter.get_civ_list())
     except HTTPException:
         # this occurs when civ_list is empty; ctx.send can't send an empty message
-        await ctx.send(f"HTTPException: The civ list is empty. Make sure you ran `{prefix}start {{ban_limit}} (optional)` first.")
+        await ctx.send(
+            f"HTTPException: The civ list is empty. Make sure you ran `{prefix}start {{ban_limit}} (optional)` first.")
+
 
 @bot.command()
 async def playerlist(ctx):
@@ -132,7 +148,8 @@ async def playerlist(ctx):
         await ctx.send(drafter.get_player_list())
     except HTTPException:
         # this occurs when player_list is empty; ctx.send can't send an empty message
-        await ctx.send(f"HTTPException: The player list is empty. Make sure you ran `{prefix}start {{ban_limit}} (optional)` first.")
+        await ctx.send(
+            f"HTTPException: The player list is empty. Make sure you ran `{prefix}start {{ban_limit}} (optional)` first.")
 
 
 @bot.command()
@@ -141,7 +158,8 @@ async def bancount(ctx):
         await ctx.send(drafter.get_player_bans())
     except HTTPException:
         # this occurs when player_list is empty; ctx.send can't send an empty message
-        await ctx.send(f"HTTPException: The ban count list is empty. Make sure you ran `{prefix}start {{ban_limit}} (optional)` first.")
+        await ctx.send(
+            f"HTTPException: The ban count list is empty. Make sure you ran `{prefix}start {{ban_limit}} (optional)` first.")
 
 
 @bot.command()
@@ -160,6 +178,7 @@ async def removeplayer(ctx, player: str):
         await ctx.send(f"**{player}** is not in the player_bans dictionary. Cedric, debug this.")
         raise KeyError
 
+
 @bot.command()
 async def addplayer(ctx, player: str):
     # enforce uniqueness by checking player_list
@@ -169,9 +188,13 @@ async def addplayer(ctx, player: str):
     else:
         await ctx.send(f"**{player}** is already in the list.")
 
+
 @bot.command()
 async def help(ctx):
-    embed = discord.Embed(title="Drafter Commands", description=f"- `{prefix}start {{ban_limit}} (optional)`: Initialize the list of players and the list of civs.\n- `{prefix}ban {{civ_name}}`: Remove specified civ from list of draftable civs. Limited to 2 per player. \n- `{prefix}draft`: Generate the civ picks for each person.\n- `{prefix}civlist`: Print the list of draftable civs.\n- `{prefix}playerlist`: Print the list of players who will be assigned civ picks.\n- `{prefix}bancount`: Print the number of civilizations banned by each player. \n- `{prefix}removeplayer {{player_name}}`: Remove specified player from list of players who will be assigned civ picks. \n- `{prefix}addplayer {{player_name}}`: Add specified player to list of players who will be assigned civ picks.", color=discord.Color.blue())
+    embed = discord.Embed(title="Drafter Commands",
+                          description=f"- `{prefix}start {{ban_limit}} (optional)`: Initialize the list of players and the list of civs.\n- `{prefix}ban {{civ_name}}`: Remove specified civ from list of draftable civs. Limited to 2 per player. \n- `{prefix}draft`: Generate the civ picks for each person.\n- `{prefix}civlist`: Print the list of draftable civs.\n- `{prefix}playerlist`: Print the list of players who will be assigned civ picks.\n- `{prefix}bancount`: Print the number of civilizations banned by each player. \n- `{prefix}removeplayer {{player_name}}`: Remove specified player from list of players who will be assigned civ picks. \n- `{prefix}addplayer {{player_name}}`: Add specified player to list of players who will be assigned civ picks.",
+                          color=discord.Color.blue())
     await ctx.send(embed=embed)
+
 
 bot.run(TOKEN)
